f0bda5b1964727fb27205d48b045b39a9a979390
Vadym Davydenko <ak0lenfs@gmail.com>
2022-12-22 11:34:20 +0100

    allocate memory for char arr

--- main.c
+++ main.c
@@ -5,8 +5,15 @@
 #define MAX_LINES 9
 #define MAX_LEN 100
 
+char **reader(){
+    char **arr = (char**)malloc(sizeof(char*));
+    for (int i = 0; i < MAX_LINES; i++){
+        arr[i] = (char*)malloc(MAX_LEN);
+    }
+}
+
 int main(){
 
-    
+
     return 0;
 }
