57e672b210695de85a98a649280938b3e7726d45
Vadym Davydenko <ak0lenfs@gmail.com>
2022-12-22 11:47:52 +0100

    created reader func
    reads raw data for each CPU core and sends it to the analyzer..

--- main.c
+++ main.c
@@ -6,14 +6,37 @@
 #define MAX_LEN 100
 
 char **reader(){
-    char **arr = (char**)malloc(sizeof(char*));
+    char **arr = (char**)malloc(sizeof(char*) * MAX_LINES);
     for (int i = 0; i < MAX_LINES; i++){
         arr[i] = (char*)malloc(MAX_LEN);
     }
+
+    FILE* fp;
+
+    if ((fp = fopen("/proc/stat", "r")) == NULL){
+        printf("File cannot be opened!\n");
+        exit(1);
+    }
+
+    int line = 0;
+    while (!feof(fp) && !ferror(fp) && line < MAX_LINES){
+        if (fgets(arr[line], MAX_LEN, fp) != NULL){
+            line++;
+        }
+    }
+    fclose(fp);
+
+    return arr;
 }
 
 int main(){
+    char **data = NULL;
+    
+    data = reader();
 
+    for (int i = 0; i < MAX_LINES; i++){
+        printf("%s", data[i]);
+    }
 
     return 0;
 }
